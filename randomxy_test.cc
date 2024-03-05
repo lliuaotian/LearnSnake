332d61258eb2a207828d060a82fe177f25a50249
lliuaotian <liuaotianmsj@gmail.com>
2024-03-04 19:23:41 -0800

    先清空仓库，自己重新写

--- randomxy_test.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "randomxy.h"
-
-int main (int argc, char *argv[]) {
-  Random rand;
-  std::cout << rand.get_x(0, 10) << "," << rand.get_y(0, 10) << std::endl;
-
-  return 0;
-}
