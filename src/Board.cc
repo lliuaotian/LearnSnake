e96774e47d8c4b8d67ad0e9c1e0ae953c00626bd
lliuaotian <liuaotianmsj@gmail.com>
2024-03-07 01:04:27 -0800

    修复if判断使用了固定值9，还有棋盘的width height改成了默认+2，假设用户传递过来的就是蛇能跑的宽度和高度，忽视掉两侧的墙壁

--- src/Board.cc
+++ src/Board.cc
@@ -9,18 +9,20 @@
    3是食物
    */
 
+// width+2 height+2 是因为棋盘的第一行、列 最后一行、列是墙壁
 Board::Board(int width, int height) :
   _width(width),
   _height(height),
-  _boardMap(width, std::vector<int>(height)) {
+  _boardMap(width + 2, std::vector<int>(height + 2)) {
   // 将棋盘初始化一下，四周都是墙壁，中间是空
+  // 刚开始两个if判断，or后都是固定值9，但是这样如果传递到该类的width和height不是10就是错误，更改了这个出现错误的可能
   for (int i = 0; i < _boardMap.size(); ++i) {
     for (int j = 0; j < _boardMap[i].size(); ++j) {
-      if (i == 0 or i == 9) {
+      if (i == 0 or i == _boardMap.size() - 1) {
         _boardMap[i][j] = 1;
         continue;
       }
-      if (j == 0 or j == 9) {
+      if (j == 0 or j == _boardMap[i].size() - 1) {
         _boardMap[i][j] = 1;
         continue;
       }
