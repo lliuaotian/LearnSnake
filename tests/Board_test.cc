3aad33b362d6f86f726cd3f6d764f87f159c4ae4
lliuaotian <liuaotianmsj@gmail.com>
2024-04-27 03:08:16 -0700

    增加对新版Board.cc的test支持

--- tests/Board_test.cc
+++ tests/Board_test.cc
@@ -1,8 +1,9 @@
 #include "../src/Board.h"
 #include <iostream>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   Board board(10, 10);
+  Board_state board_element;
 
   BoardCoordinate a;
   a.x = 3;
@@ -10,16 +11,27 @@ int main (int argc, char *argv[]) {
 
   board.draw();
 
-  board.setCell(a, 1);
+  board.setCell(a, Snake_head);
 
   board.draw();
 
   a.x = 4;
   a.y = 5;
-  board.setCell(a, 1);
+  board.setCell(a, Snake_body);
 
   board.draw();
 
+  a.x = 7;
+  a.y = 7;
+  board.setCell(a, Food);
+
+  board.draw();
+
+  a.x = 10;
+  a.y = 10;
+  board.setCell(a, Food);
+
+  board.draw();
 
   std::cout << "获取的坐标位置的数据是:" << board.getCell(a) << std::endl;
 
